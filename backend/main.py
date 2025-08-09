edf174ae2aefda010eb78736644fab6f2d907b48
Anubhav <singhkrishna6052@gmail.com>
2025-08-09 08:32:50 +0530

    created a main.py with a post to get the data via registratiion

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,38 @@
-from fastapi import FastAPI, File, UploadFile
-import fitz  # PyMuPDF
-import io
+from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from models import UserCreate, UserLogin
+from auth import hash_password, verify_password, create_access_token, decode_access_token
+from database import users_collection
+from bson import ObjectId
+
 
 app = FastAPI()
 
-@app.post("/parse-pdf/")
-async def parse_pdf(file: UploadFile = File(...)):
-    contents = await file.read()
-    text = extract_text_from_pdf(contents)
-    return {"text": text}
+models.Base.metadata.create_all(bind=database.engine)
 
-def extract_text_from_pdf(file_bytes: bytes) -> str:
-    text = ""
-    with fitz.open(stream=file_bytes, filetype="pdf") as doc:
-        for page in doc:
-            text += page.get_text()
-    return text.strip()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React app URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+@app.post("/register")
+def register(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    new_user = models.User(
+        email=user.email,
+        hashed_password=auth.hash_password(user.password)
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return {"msg": "Registration successful"}
+
