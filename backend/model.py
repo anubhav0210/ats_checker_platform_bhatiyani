acdae7efe56cf78aa710f02c0599fd169459aab6
Anubhav <singhkrishna6052@gmail.com>
2025-08-09 08:20:30 +0530

    created a model.py with structure for basic authentication

---
+++ backend/model.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserInDB(BaseModel):
+    email: EmailStr
+    hashed_password: str
