7d7ce4e88fc66717f79d8bdde9e744cd57563722
Anubhav <singhkrishna6052@gmail.com>
2025-08-10 00:10:01 +0530

    created a database.py where you store url and collection for mongodb database

---
+++ backend/database.py
@@ -0,0 +1,18 @@
+# database.py
+# MongoDB connection using Motor (async client)
+
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017/resume_ats")
+DB_NAME = os.environ.get("DB_NAME", "resume_ats")
+
+client: AsyncIOMotorClient = AsyncIOMotorClient(MONGO_URI)
+db = client[DB_NAME]
+
+# Collections recommended
+users_collection = db["users"]
+resumes_collection = db["resumes"]
