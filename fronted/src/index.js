630c03da55543dc8fdb6c638a153210c3871a0b6
Anubhav <singhkrishna6052@gmail.com>
2025-08-09 16:24:58 +0530

    chaged the component from browserRoutes to AuthProvider

--- fronted/src/index.js
+++ fronted/src/index.js
@@ -2,16 +2,17 @@
 // index.js
 // Entry point of the React application. It renders the App component.
 
+// src/index.js (wrap <App/> in AuthProvider)
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <AuthProvider>
     <App />
-  </BrowserRouter>
+  </AuthProvider>
 );
 
 
