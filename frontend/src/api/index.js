69dbb60601b27825d0b125736ebec51331f71206
Anubhav <singhkrishna6052@gmail.com>
2025-08-12 00:51:24 +0530

    update frontend folder

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -5,5 +5,6 @@ import { mockBackend } from './mockBackend';
 // Change this to switch between backends
 const USE_MOCK = process.env.REACT_APP_USE_MOCK === 'true';
 
+
 export const authAPI = USE_MOCK ? mockBackend.authAPI : realBackend.authAPI;
 export const resumeAPI = USE_MOCK ? mockBackend.resumeAPI : realBackend.resumeAPI;
